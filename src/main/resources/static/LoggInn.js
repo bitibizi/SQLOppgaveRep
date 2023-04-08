8d0e4733c7360bda15f08edc68d345e50d5a6116
bitibizi <hanbit199@gmail.com>
2023-04-08 15:56:09 +0200

    byttet fra onchange to id on login

--- src/main/resources/static/LoggInn.js
+++ src/main/resources/static/LoggInn.js
@@ -1,22 +1,22 @@
-loggInn=()=>{
-
-    const bruker={
-        brukernavn: $("#brukerNavn").val(),
-        password: $("#password").val()
-    }
-
-    $.post("/loggInn", bruker, function(innlogget){
-        if(innlogget){
-            window.location.href='List.html'
+$(() => {
+    $("#login-button").click(event => {
+        event.preventDefault()
+        const bruker = {
+            brukernavn: $("#brukerNavn").val(),
+            password: $("#password").val()
         }
-        else{
 
-            $("#feil").html("Feil brukernavn eller password");
-        }
-    })
-        .fail(function(){
-            $("#feil").html("Serverfeil-prøv igjen senere");
+        $.post("/loggInn", bruker, function (innlogget) {
+            if (innlogget) {
+                window.location.href = 'List.html'
+            } else {
+
+                $("#feil").html("Feil brukernavn eller password");
+            }
         })
+            .fail(function () {
+                $("#feil").html("Serverfeil-prøv igjen senere");
+            })
 
-}
-
+    })
+})
